73cd9be3bd7fb2875496870f5732a81394b6e051
Akira_Endo <verniy_4794@outlook.jp>
2023-11-14 14:54:59 +0900

    feat::ddsファイルの作成プログラム

--- TextureConverter.cpp
+++ TextureConverter.cpp
@@ -5,12 +5,69 @@ void TextureConverter::ConverterTextureWICToDDS(const std::string& filePath)
 	//テクスチャファイルを読み込む
 	LoadWICTextureFromFile(filePath);
 	
+	SaveDDSTextureToFile();
 }
 
 void TextureConverter::LoadWICTextureFromFile(const std::string& filePath)
 {
 	//ファイルパスをワイド文字列に変換する
 	std::wstring wfilePath = ConverterMultiByteStringToWideString(filePath);
+
+	//WICテクスチャのロード
+	HRESULT result = LoadFromWICFile(wfilePath.c_str(), WIC_FLAGS_NONE, &metaData_, scratchImage_);
+	assert(SUCCEEDED(result));
+
+	// フォルダパスとファイル名を分離する
+	SeparateFilePath(wfilePath);
+
+}
+
+void TextureConverter::SeparateFilePath(const std::wstring& filePath)
+{
+	size_t pos1;
+	std::wstring exceptExt;
+
+	// 区切り文字 '.'が出てくる一番最後の部分を検索
+	pos1 = filePath.rfind('.');
+
+	// 検索ヒットしたら
+	if (pos1 != std::wstring::npos) {
+		// 区切り文字の後ろをファイル拡張子として保存
+		fileExt_ = filePath.substr(pos1 + 1, filePath.size() - pos1 - 1);
+
+		// 区切りの文字の前までを抜き出す
+		exceptExt = filePath.substr(0, pos1);
+	}
+	else {
+		fileExt_ = L"";
+		exceptExt = filePath;
+	}
+
+	// 区切り文字 '\\' が出てくる一番最後の部分を検索
+	pos1 = exceptExt.rfind('\\');
+	if (pos1 != std::wstring::npos) {
+		// 区切り文字の前までをディレクトリパスとして保存
+		directoryPath_ = exceptExt.substr(0, pos1 + 1);
+
+		// 区切り文字の後ろをファイル名として保存
+		fileName_ = exceptExt.substr(pos1 + 1, exceptExt.size() - pos1 - 1);
+		return;
+	}
+
+	// 区切り文字 '/' が出てくる一番最後の部分を検索
+	pos1 = exceptExt.rfind('/');
+	if (pos1 != std::wstring::npos) {
+		// 区切り文字の前までをディレクトリパスとして保存
+		directoryPath_ = exceptExt.substr(0, pos1 + 1);
+
+		// 区切り文字の後ろをファイル名として保存
+		fileName_ = exceptExt.substr(pos1 + 1, exceptExt.size() - pos1 - 1);
+		return;
+	}
+
+	// 区切り文字がないのでファイル名のみとして扱う
+	directoryPath_ = L"";
+	fileName_ = exceptExt;
 }
 
 std::wstring TextureConverter::ConverterMultiByteStringToWideString(const std::string& mString)
@@ -27,4 +84,19 @@ std::wstring TextureConverter::ConverterMultiByteStringToWideString(const std::s
 
 
 	return wString;
+}
+
+void TextureConverter::SaveDDSTextureToFile()
+{
+	HRESULT result;
+
+	// 読み込んだテクスチャをSRGBとして扱う
+	metaData_.format = DirectX::MakeSRGB(metaData_.format);
+
+	// 出力ファイル名を設定する
+	std::wstring filePath = directoryPath_ + fileName_ + L".dds";
+
+	// DDSファイル書き出し
+	result = SaveToDDSFile(scratchImage_.GetImages(), scratchImage_.GetImageCount(), metaData_, DirectX::DDS_FLAGS_NONE, filePath.c_str());
+	assert(SUCCEEDED(result));
 }
--- TextureConverter.h
+++ TextureConverter.h
@@ -3,20 +3,59 @@
 #include "windows.h"
 #include <DirectXTex.h>
 
+using namespace DirectX;
+
+
 class TextureConverter
 {
 public:
-
+	/// <summary>
+	/// テクスチャをWICからDDSに変換する
+	/// </summary>
+	/// <param name="filePath">ファイルパス</param>
 	void ConverterTextureWICToDDS(const std::string& filePath);
 
-private:
-
+	/// <summary>
+	/// テクスチャ読み込み
+	/// </summary>
+	/// <param name="filePath">ファイルパス</param>
 	void LoadWICTextureFromFile(const std::string& filePath);
 
+	/// <summary>
+	/// フォルダパスとファイル名を分離する
+	/// </summary>
+	/// <param name="filePath">ファイルパス</param>
+	void SeparateFilePath(const std::wstring& filePath);
+
+
+
 
 private:
 
+	/// <summary>
+	/// マルチバイト文字列をワイド文字列に変換
+	/// </summary>
+	/// <param name="mString">マルチバイト文字列</param>
+	/// <returns>ワイド文字列</returns>
 	static std::wstring ConverterMultiByteStringToWideString(const std::string& mString);
+
+private:
+	/// <summary>
+	/// DDSファイルとしてファイル書き出し
+	/// </summary>
+	void SaveDDSTextureToFile();
+
+private:
+	//画像の情報
+	DirectX::TexMetadata metaData_;
+	//画像イメージのコンテナ
+	DirectX::ScratchImage scratchImage_;
+	//ディレクトリパス
+	std::wstring directoryPath_;
+	//ファイル名
+	std::wstring fileName_;
+	//ファイル拡張子
+	std::wstring fileExt_;
 
 
 };
